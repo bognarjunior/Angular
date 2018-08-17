4c221fc66fa0a8455fa2714ed7c03f4f732fe603
Bognar Junior <bognar_junior@yahoo.com.br>
2018-08-16 22:54:46 -0300

    Criando funções
    Criando funçõespara ligar o component com o service

--- calculadora/src/app/calculadora/components/calculadora.component.ts
+++ calculadora/src/app/calculadora/components/calculadora.component.ts
@@ -8,11 +8,90 @@ import { CalculadoraService } from './../services';
 })
 export class CalculadoraComponent implements OnInit {
 
+  private n1: string;
+  private n2: string;
+  private resultado: number;
+  private operacao: string;
+
   constructor(
     private calculadoraService: CalculadoraService
   ) { }
 
   ngOnInit() {
+    this.limpar();
   }
 
+  limpar(): void {
+    this.n1 = '0';
+    this.n2 = null;
+    this.resultado = null;
+    this.operacao = null;
+  }
+
+  adicionaNumero(n: string): void {
+    if(this.operacao === null) {
+      this.n1 = this.concatenarNumero(this.n1, n);
+    } else {
+      this.n2 = this.concatenarNumero(this.n2, n);
+    }
+  }
+
+  concatenarNumero(nAtual: string, nConcat: string): string {
+    if (nAtual === '0' || nAtual === null) {
+      nAtual = '';
+    }
+
+    if (nConcat === '.' && nAtual === '0') {
+      return '0.';
+    }
+
+    if (nConcat === '.' && nAtual.indexOf('.') > -1 ) {
+      return nAtual;
+    }
+    return nAtual + nConcat;
+  }
+
+  definiOperacao(operacao: string): void {
+    if (this.operacao === null) {
+      this.operacao = operacao;
+      return;
+    }
+
+    if(this.n2 !== null) {
+      this.resultado = this.calculadoraService.calcular(
+        parseFloat(this.n1),
+        parseFloat(this.n2),
+        this.operacao
+      );
+
+      this.n1 = this.resultado.toString();
+      this.n2 = null;
+      this.resultado = null;
+      this.operacao = operacao;
+    }
+  }
+
+  calcular(): void {
+    if (this.n2 === null) {
+      return;
+    }
+
+    this.resultado = this.calculadoraService.calcular(
+      parseFloat(this.n1),
+      parseFloat(this.n2),
+      this.operacao
+    );
+  }
+
+  get display(): string {
+    if (this.resultado !== null) {
+      return this.resultado.toString();
+    }
+
+    if (this.n2 !== null) {
+      return this.n2;
+    }
+
+    return this.n1;
+  } 
 }
